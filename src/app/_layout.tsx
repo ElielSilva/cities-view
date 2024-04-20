b2236c74d653c0eef8b85dcf910002a852bf36ba
ElielSilva <silvaeliel940@gmail.com>
2024-04-20 20:14:37 -0300

    feat: created folders with project structure as suggested in the official expo documentation

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+    
+
+  return(
+    <Stack>
+      <Stack.Screen name="index" options={{title: "Login"}} />
+      <Stack.Screen name="home/index" options={{title: "tela principal"}} />
+      <Stack.Screen name="citydetails/index" options={{title: "Detalhes da cidade"}} />
+    </Stack>
+  );
+}
